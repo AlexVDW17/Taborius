d1132ea7f31c7a64dc347fbd4722837ea7de7ec7
Alex Van De Wiele <57680806+AlexVDW17@users.noreply.github.com>
2020-01-15 23:29:57 -0500

    Add files via upload

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+    public GameObject colorChanging;
+    public Material mycolor;
+    // Use this for initialization
+    public void changeItscolor()
+    {
+
+        colorChanging.GetComponent<Renderer>().material.color = mycolor.color;
+
+
+    }
+}
---
+++ Assets/MoveOn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveOn : MonoBehaviour {
+
+    public GameObject Head, Body, Gun, Sphere;
+
+   public void MovinOn()
+    {
+        Storage.Head = Head.GetComponent<Renderer>().material.color;
+        Storage.Body = Body.GetComponent<Renderer>().material.color;
+        Storage.Gun = Gun.GetComponent<Renderer>().material.color;
+        Storage.Sphere = Sphere.GetComponent<Renderer>().material.color;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("WhoAreYou.unity");
+    }
+	
+}
